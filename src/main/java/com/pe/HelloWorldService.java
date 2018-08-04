95b1af66b3c1a6578d1a9d3571659adb52bacc80
Dieg0407 <dieg0407@hotmail.com>
2018-08-04 16:42:53 -0500

    test

--- src/main/java/com/pe/HelloWorldService.java
+++ src/main/java/com/pe/HelloWorldService.java
@@ -1,7 +1,6 @@
-package com.pe.azoth;
+package com.pe;
  
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -9,8 +8,8 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.core.Response;
   
-@Path("/test")
-public class Test {
+@Path("/hello")
+public class HelloWorldService {
   
     @GET
     @Path("/{name}")
@@ -31,15 +30,7 @@ public class Test {
         String output = String.format("%s : %s %s" , "Hola",name,lname);
   
         return Response.status(200).entity(output).build();
-    }
-
-    @POST
-    @Path("/testLogin")
-    public Response getCredentials(
-        @QueryParam("user") String user,
-        @QueryParam("pass") String pass
-        ){
-        return Response.status(200).entity("1321321.asavava.aceptado").build();
+  
     }
 }
   
